ab67bec0fa7dfc37b25098568360ce0316682d9f
Shahzad <mech1090@gmail.com>
2022-04-16 15:44:22 -0600

    Can not get render

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const pug = require('pug')
 const app = express()
+app.use(express.static('public'))
 
 app. set('view engine', 'pug')
 app.set('views','./views')
 app.get('/',(req,res)=>{
     res.send('OK')
 })
+
+app.get('/Demo',(req,res)=>{
+    console.log('IN RENDER AREA')
+    res.render('index.pug',{
+        pageTitle : 'restaurant main page'
+    })
+})
+
 app.listen(3000,()=>{
     console.log('Server is running on Port 3000')
 })
