719f32dd67fe2458487dd8a274ec145667781047
Shahzad <mech1090@gmail.com>
2022-04-18 16:44:59 -0600

    can not post the form

--- index.js
+++ index.js
@@ -2,31 +2,41 @@ const express = require('express')
 const pug = require('pug')
 const app = express()
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 app. set('view engine', 'pug')
 app.set('views','./views')
+
+
 app.get('/',(req,res)=>{
     res.send('OK')
 })
 
+
 app.get('/Demo',(req,res)=>{
     console.log('IN RENDER AREA')
     res.render('restaurDemo/layout',{
-        pageTitle:'Paninos',
+        pageTitle:'Panini',
         isauthenticated: true,
         pageHeader:'Gourmet Italian Grilled Sandwiches & More!'
 
     })
 })
 
+app.post('/register',(req,res)=>{
+    console.log(req.body)
+})
 
 app.get('/register',(req,res)=>{
-    console.log('IN LOGIN AREA')
+    console.log('IN Register Area')
     res.render('register/layout1',{
         pageHeader:'Gourmet Italian Grilled Sandwiches & More!'
     })
     
 })
+
+
+
 
 app.listen(3000,()=>{
     console.log('Server is running on Port 3000')
