1e19036b5ff62b5d324cf048615758e42afd2c53
Shahzad <mech1090@gmail.com>
2022-04-25 14:58:01 -0600

    registration

--- model/Registration.js
+++ model/Registration.js
@@ -4,20 +4,18 @@ const schemaDefination = {
         type:String,
         required : [true,'name is Required']
     },
-    mobile:{
-        type:Number,
-        required:[true,'mobile number is Required']
-    },
     email:{
         type:String,
         required : [true,'Email is Required']
     },
-    
+    mobile:{
+        type:Number,
+        required:[true,'mobile number is Required']
+    },
     message:{
         type:String
     }
 }
 const registrationSchema = new mongoose.Schema(schemaDefination)
 const Registration = mongoose.model('Registration',registrationSchema)
-
 module.exports = Registration
